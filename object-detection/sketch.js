dda310a2db7afde18324ddbb7e0ff5756c938bc1
Finley Dabinett <finleytorterra@gmail.com>
2021-10-20 17:31:28 +0100

    Tweaks

--- object-detection/sketch.js
+++ object-detection/sketch.js
@@ -2,8 +2,8 @@ let img
 let detector
 
 function preload () {
-	img = loadImage('images/dog1.jpg')
-	detector = ml5.objectDetector('cocossd')
+	img = loadImage('images/dogs.jpeg')
+	detector = ml5.objectDetector('cocossd') 
 }
 
 function setup () {
@@ -11,13 +11,14 @@ function setup () {
 	image(img, 0, 0)
 	textSize(16)
 	strokeWeight(2)
-	stroke(0, 255, 100)
 	detector.detect(img, (error, results) => {
 		if (error) console.error(error)
 		console.log(results)
 		for (let i = 0; i < results.length; i++) {
 			noFill()
+			stroke(0, 255, 100)
 			rect(results[i].x, results[i].y, results[i].width, results[i].height)
+			noStroke()
 			fill(0, 255, 100)
 			text(results[i].label, results[i].x + 10, results[i].y + 20)
 		}
